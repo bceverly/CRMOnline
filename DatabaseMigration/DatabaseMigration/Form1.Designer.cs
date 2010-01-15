50abd27189e7596ed68425a4aa94f326a7bca37c
Bryan Everly <bryan@bceassociates.com>
2010-01-15 15:27:04 -0500

    Initial checkin - database migration tool

---
+++ DatabaseMigration/DatabaseMigration/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace DatabaseMigration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbDatabaseServer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbUserId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btnMigrate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Database Server:";
+            // 
+            // tbDatabaseServer
+            // 
+            this.tbDatabaseServer.Location = new System.Drawing.Point(110, 13);
+            this.tbDatabaseServer.Name = "tbDatabaseServer";
+            this.tbDatabaseServer.Size = new System.Drawing.Size(100, 20);
+            this.tbDatabaseServer.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "User ID:";
+            // 
+            // tbUserId
+            // 
+            this.tbUserId.Location = new System.Drawing.Point(110, 45);
+            this.tbUserId.Name = "tbUserId";
+            this.tbUserId.Size = new System.Drawing.Size(100, 20);
+            this.tbUserId.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password:";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(110, 78);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.TabIndex = 5;
+            // 
+            // btnMigrate
+            // 
+            this.btnMigrate.Location = new System.Drawing.Point(96, 124);
+            this.btnMigrate.Name = "btnMigrate";
+            this.btnMigrate.Size = new System.Drawing.Size(75, 23);
+            this.btnMigrate.TabIndex = 6;
+            this.btnMigrate.Text = "Migrate";
+            this.btnMigrate.UseVisualStyleBackColor = true;
+            this.btnMigrate.Click += new System.EventHandler(this.btnMigrate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 161);
+            this.Controls.Add(this.btnMigrate);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbUserId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbDatabaseServer);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Database Migration Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbDatabaseServer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbUserId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btnMigrate;
+    }
+}
+
---
+++ DatabaseMigration/DatabaseMigration/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DatabaseMigration
+{
+    class Tables
+    {
+        string TABLE_NAME { get; set; }
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMigrate_Click(object sender, EventArgs e)
+        {
+            string connectionString = string.Format("Data Source={0};Initial Catalog=master;User ID={1};Password={2}",
+                tbDatabaseServer.Text, tbUserId.Text, tbPassword.Text);
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            DataClasses1DataContext dc = new DataClasses1DataContext(connection);
+
+            try
+            {
+                var results = dc.ExecuteQuery<Tables>(@"SELECT TABLE_NAME from INFORMATION_SCHEMA.TABLES");
+            }
+            catch (Exception theException)
+            {
+                MessageBox.Show(string.Format("Error:  {0}", theException.Message));
+                return;
+            }
+        }
+    }
+}
